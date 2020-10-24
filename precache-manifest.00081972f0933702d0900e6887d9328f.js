daff10bb6987291080c0a816310e7b638615cc2a
jeswinjoseph123 <jeswinjoseph08@gmail.com>
2020-10-24 22:38:05 +0530

    Updates

---
+++ precache-manifest.00081972f0933702d0900e6887d9328f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18451ee57c2644ff1b4d728725c079e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0d1c6be9aba9f8adbe",
+    "url": "/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "98619f46be6174e345cb",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bc0d1c6be9aba9f8adbe",
+    "url": "/static/js/2.eab2d136.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.eab2d136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98619f46be6174e345cb",
+    "url": "/static/js/main.5f10a3d5.chunk.js"
+  },
+  {
+    "revision": "b711ace63aa913999568",
+    "url": "/static/js/runtime-main.a0f21f5b.js"
+  }
+]);
